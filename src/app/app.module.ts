fe89e75cfd9b7f2aabbcfff41c30d9871e4898d5
Romserg <serg.romankov@gmail.com>
2019-10-08 14:26:45 +0300

    Add ngx-bootstrap date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
@@ -21,7 +23,9 @@ const appRoutes: Routes = [
 	],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
     FormsModule,
+		BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes)
 	],
 	providers: [],
