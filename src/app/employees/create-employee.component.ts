34e989ab6bb79a40c2570c59a67d4c2ff6fe1705
Romserg <serg.romankov@gmail.com>
2019-10-08 20:31:06 +0300

    Add dynamic required attribute to phone/email fields

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -2,44 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Employee } from '../models/employee.model';
 
 @Component({
-	selector: 'app-create-employee',
-	templateUrl: './create-employee.component.html',
-	styleUrls: ['./create-employee.component.css']
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-	previewPhoto = false;
-	dateOfBirth: Date = new Date(2018,0,30);
-	datePickerConfig: Partial<BsDatepickerConfig>;
-	departments: Department[] = [
-		{id: 1, name: 'Help Desk'},
-		{id: 2, name: 'HR'},
-		{id: 3, name: 'IT'},
-		{id: 4, name: 'Payroll'},
-		{id: 5, name: 'Admin'},
-	];
+  previewPhoto = false;
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: '',
+    dateOfBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null
+  };
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Payroll'},
+    {id: 5, name: 'Admin'},
+  ];
 
-	constructor() {
-		this.datePickerConfig = Object.assign({},
-			{
-				containerClass: 'theme-dark-blue',
-				minDate: new Date(1900,0,1),
-				maxDate: new Date(2019,11,31),
-				dateInputFormat: 'DD/MM/YYYY',
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        dateInputFormat: 'DD/MM/YYYY',
+      })
+  }
 
-			})
-	}
+  ngOnInit() {
+  }
 
-	ngOnInit() {
-	}
+  saveEmployee(newEmployee: Employee): void {
+    console.log(newEmployee)
+  }
 
-	saveEmployee(empForm: NgForm): void {
-		console.log(empForm.value)
-	}
-
-	togglePhotoPreview(){
-		this.previewPhoto = !this.previewPhoto;
-	}
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
 
 }
