236d2235edda3f60471ea76758fb00b41a4bffb9
Romserg <serg.romankov@gmail.com>
2019-10-10 19:44:36 +0300

    Add possibility to switch employees

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -7,7 +7,16 @@ import { Employee } from "../models/employee.model";
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit {
-  @Input() employee: Employee;
+  private _employee: Employee;
+  @Input()
+  set employee(val: Employee) {
+    console.log('Previous: ' + (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current: ' + val.name);
+    this._employee = val;
+  };
+  get employee() :Employee {
+    return this._employee;
+  }
   constructor() { }
 
   ngOnInit() {
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -3,17 +3,30 @@ import { Employee } from '../models/employee.model';
 import { EmployeeService } from "./employee.service";
 
 @Component({
-	templateUrl: './list-employees.component.html',
-	styleUrls: ['./list-employees.component.css']
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
 })
 export class ListEmployeesComponent implements OnInit {
-	employees: Employee[];
+  employees: Employee[];
+  employeeToDisplay: Employee;
+  private arrayIndex = 1;
 
-	constructor(private _employeeService: EmployeeService) {
-	}
+  constructor(private _employeeService: EmployeeService) {
+  }
 
-	ngOnInit() {
-	  this.employees = this._employeeService.getEmployees();
-	}
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.employeeToDisplay = this.employees[0];
+  }
+
+  nextEmployee(): void {
+    if (this.arrayIndex <= 2) {
+      this.employeeToDisplay = this.employees[this.arrayIndex];
+      this.arrayIndex += 1;
+    } else {
+      this.employeeToDisplay = this.employees[0];
+      this.arrayIndex = 1;
+    }
+  }
 
 }
