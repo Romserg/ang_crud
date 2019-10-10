33f21224a65e09282bab6d695948226ab8b0852e
Romserg <serg.romankov@gmail.com>
2019-10-10 22:52:55 +0300

    Add @Input set and get method for single property

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -7,17 +7,29 @@ import { Employee } from "../models/employee.model";
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit {
+  private _employeeId: number;
+  @Input()
+  set employeeId(val: number) {
+    console.log('employeeId changed from ' + JSON.stringify(this._employeeId) + ' to '+ JSON.stringify(val));
+    this._employeeId = val;
+  };
+  get employeeId(): number {
+    return this._employeeId;
+  }
+
   private _employee: Employee;
   @Input()
   set employee(val: Employee) {
-    console.log('Previous: ' + (this._employee ? this._employee.name : 'NULL'));
-    console.log('Current: ' + val.name);
+    console.log('employee changed from ' + JSON.stringify(this._employee) + ' to '+ JSON.stringify(val));
     this._employee = val;
   };
-  get employee() :Employee {
+
+  get employee(): Employee {
     return this._employee;
   }
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
