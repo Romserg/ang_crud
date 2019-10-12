3217ed03c8a8f030da2353191b80b252c8a688e4
Romserg <serg.romankov@gmail.com>
2019-10-12 18:26:34 +0300

    Add query params to url

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -41,6 +41,10 @@ export class ListEmployeesComponent implements OnInit {
   }
 
   onClick(employeeId: number) {
-    this._router.navigate(['/employees', employeeId])
+    this._router.navigate(['/employees', employeeId], {
+      queryParams: {
+        'searchTerm': this.searchTerm
+      }
+    })
   }
 }
