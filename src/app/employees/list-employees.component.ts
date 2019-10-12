f761544b131c422db685c5d57caa06c4135ff353
Romserg <serg.romankov@gmail.com>
2019-10-12 18:06:11 +0300

    Move filtering logic into component

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -9,15 +9,38 @@ import { Router } from "@angular/router";
 })
 export class ListEmployeesComponent implements OnInit {
   employees: Employee[];
-  searchTerm: string;
+  filteredEmployees: Employee[];
+  private _searchTerm: string;
+
+  get searchTerm(): string {
+    return this._searchTerm
+  }
+
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
+
+  filterEmployees(searchString: string) {
+    return this.employees.filter(employee => {
+      return employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1
+    })
+  }
+
   constructor(private _employeeService: EmployeeService, private _router: Router) {
   }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.filteredEmployees = this.employees;
   }
 
-  onClick(employeeId: number){
+  changeEmployeeName() {
+    this.employees[0].name = 'Jordan';
+    this.filteredEmployees = this.filterEmployees(this.searchTerm);
+  }
+
+  onClick(employeeId: number) {
     this._router.navigate(['/employees', employeeId])
   }
 }
